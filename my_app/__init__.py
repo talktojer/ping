27f9b3dc1054cd0a81776a5aa34aef3b48008ea9
Jeremy Hancock <talktojer@gmail.com>
2023-09-10 16:33:13 -0400

    update

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -10,3 +10,19 @@ from my_app.routes import auth_routes, admin_routes
 
 app.register_blueprint(auth_routes)
 app.register_blueprint(admin_routes)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    approved = db.Column(db.Boolean, default=False)
+    is_logged_in = db.Column(db.Boolean, default=False)
+    last_active = db.Column(db.DateTime, nullable=True)
+
+class SystemStatus(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    online = db.Column(db.Boolean, default=False)
+SQLALCHEMY_DATABASE_URI = 'sqlite:////usr/app/src/db/users.db'
+SESSION_TYPE = 'filesystem'
+SECRET_KEY = 'supersecretkey'
+NEW_USERS_APPROVALS = 0
