ac4ed8d791278894ee28651583e475e50b6ba462
Jeremy Hancock <talktojer@gmail.com>
2023-09-10 16:10:57 -0400

    update

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -7,5 +7,6 @@ app.config.from_object('my_app.config')
 db = SQLAlchemy(app)
 
 from my_app.routes import auth_routes, admin_routes
+
 app.register_blueprint(auth_routes)
 app.register_blueprint(admin_routes)
