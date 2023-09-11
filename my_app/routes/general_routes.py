0e5a65293d3e1b20400b0ddb053029d69e1dce39
Jeremy Hancock <talktojer@gmail.com>
2023-09-11 17:14:14 -0400

    asd

--- my_app/routes/general_routes.py
+++ my_app/routes/general_routes.py
@@ -5,8 +5,15 @@ from my_app.models import User
 import requests
 from my_app.models import SystemStatus, ChatMessage
 import logging
+import re
+from my_app.routes.openai_routes import get_completion
 
 general_routes = Blueprint('general_routes', __name__)
+def detect_bot_mention(message):
+    return bool(re.search(r"@bot", message))
+def fetch_last_n_messages(n=6):
+    return ChatMessage.query.order_by(ChatMessage.timestamp.desc()).limit(n).all()
+
 def get_active_users():
     five_minutes_ago = datetime.utcnow() - timedelta(minutes=5)
     active_users = User.query.filter(User.last_active >= five_minutes_ago).all()
@@ -60,6 +67,14 @@ def send_message():
     data = request.json
     username = data.get('username')
     message = data.get('message')
+    # Detect if the message contains @bot
+    if detect_bot_mention(message):
+        last_six_messages = fetch_last_n_messages()
+        bot_response = get_completion(last_six_messages)
+        
+        # Save bot's response to the database
+        new_bot_message = ChatMessage(username="bot", message=bot_response)
+        db.session.add(new_bot_message)
 
     new_message = ChatMessage(username=username, message=message)
     db.session.add(new_message)
