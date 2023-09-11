481ea32dbbcd02f4cedca1adb34fde587ea295a4
Jeremy Hancock <talktojer@gmail.com>
2023-09-11 17:03:06 -0400

    added openai routes

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -10,8 +10,10 @@ db = SQLAlchemy(app)
 from my_app.routes.auth_routes import auth_routes
 from my_app.routes.admin_routes import admin_routes
 from my_app.routes.general_routes import general_routes
-
+from my_app.routes.openai_routes import openai_routes
 
 app.register_blueprint(auth_routes)
 app.register_blueprint(admin_routes)
 app.register_blueprint(general_routes)
+app.register_blueprint(openai_routes)
+
---
+++ my_app/routes/openai_routes.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, request, jsonify
+import openai
+import re
+
+openai_routes = Blueprint('openai_routes', __name__)
+def get_completion(messages):
+    response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=messages,
+        temperature=0.7,
+        n=1,
+        max_tokens=500
+    )
+    return response.choices[0].message.content
+@openai_routes.route('/get_completion', methods=['POST'])
+def get_completion_route():
+    data = request.json
+    messages = data.get('messages')
+    if not messages:
+        return jsonify({'error': 'No messages provided'}), 400
+
+    completion = get_completion(messages)
+    return jsonify({'completion': completion})
