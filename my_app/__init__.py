b5dd6d2baff016730f984cd006963728ef85e170
Jeremy Hancock <talktojer@gmail.com>
2023-09-12 23:08:01 -0400

    sdf

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__, template_folder='/templates')
+app = Flask(__name__, template_folder='templates')
 app.config.from_object('my_app.config')
 
 db = SQLAlchemy(app)
+
 
 
 from my_app.routes.auth_routes import auth_routes
